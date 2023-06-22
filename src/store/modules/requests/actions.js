0c08ca6fd0b60958a90f1dadfc5db3cd1049303e
Emerson Lima <emersonlimago123@gmail.com>
2023-06-22 01:02:17 -0300

    pushing finished project to git

---
+++ src/store/modules/requests/actions.js
@@ -0,0 +1,54 @@
+export default {
+  async contactCoach(context, payload) {
+    const newRequest = {
+      userEmail: payload.email,
+      message: payload.message,
+    };
+    const response = await fetch(
+      `https://find-coach-32503-default-rtdb.firebaseio.com/requests/${payload.coachId}.json`,
+      {
+        method: 'POST',
+        body: JSON.stringify(newRequest),
+      }
+    );
+
+    const responseData = await response.json();
+    if (!response.ok) {
+      const error = new Error(
+        responseData.message || 'Failed to send request.'
+      );
+      throw error;
+    }
+
+    newRequest.id = response.name;
+    newRequest.coachId = payload.coachId;
+
+    context.commit('addRequets', newRequest);
+  },
+  async fetchRequests(context) {
+    const coachId = context.rootGetters.userId;
+    const response = await fetch(
+      `https://find-coach-32503-default-rtdb.firebaseio.com/requests/${coachId}.json`
+    );
+    const responseData = await response.json();
+    if (!response.ok) {
+      const error = new Error(
+        responseData.message || 'Failed to fetch request.'
+      );
+      throw error;
+    }
+
+    const requests = [];
+
+    for (const key in responseData) {
+      const requests = {
+        id: key,
+        coachId: coachId,
+        userEmail: responseData[key].userEmail,
+        message: responseData[key].message,
+      };
+      requests.push(requests);
+    }
+    context.commit('setRequests', requests);
+  },
+};
